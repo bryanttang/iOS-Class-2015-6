43476deff3332b2c4f4aabec0a825cb18643a3d9
bryant <bryanttgm92778@gmail.com>
2015-08-07 00:14:08 +0800

    upload exercise

---
+++ lesson6/helloTableView/helloTableView/headImageCell.h
@@ -0,0 +1,13 @@
+//
+//  headImageCell.h
+//  helloTableView
+//
+//  Created by bryant on 6/8/15.
+//  Copyright (c) 2015 cpttm. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface headImageCell : UITableViewCell
+
+@end
